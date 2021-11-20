e6e7256437b79ec5ae8ca65aa8b5a275ca6a59e1
vuptgch17406 <74758070+vuptgch17406@users.noreply.github.com>
2021-11-20 14:35:48 +0700

    bt-lesson2

---
+++ main.js
@@ -0,0 +1,25 @@
+let timeSecond = 10;
+const time = document.querySelector("h1");
+
+displayTime(timeSecond);
+
+const countDown = setInterval(() => {
+  timeSecond--;
+  displayTime(timeSecond);
+  if (timeSecond == 0 || timeSecond < 1) {
+    endCount();
+    clearInterval(countDown);
+  }
+}, 1000);
+
+function displayTime(second) {
+  const min = Math.floor(second / 60);
+  const sec = Math.floor(second % 60);
+  time.innerHTML = `
+  ${min < 10 ? "0" : ""}${min}:${sec < 10 ? "0" : ""}${sec}
+  `;
+}
+
+function endCount() {
+  time.innerHTML = "Happy New Year 2022 !!!";
+}
